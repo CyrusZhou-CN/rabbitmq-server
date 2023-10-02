d9e55cab67ec0dfabfa25908a80eb7563df22f5d
Michael Davis <mcarsondavis@gmail.com>
2023-10-02 14:34:45 -0500

    Migrate records from mnesia to Khepri with async commands
    Using async commands via Khepri's `async` command option - which
    corresponds to `ra:pipeline_command/4` - allows Ra to write multiple
    commands in a batch. This can significantly boost the migration speed
    since Ra can handle more commands per call to fsync and fsync seems to
    be the bottleneck during migration.
    
    We refactor the callbacks of the converter modules so that they take the
    overall converter module's (`rabbit_db_m2k_converter`) state record.
    That state record keeps track of the correlation IDs which are
    ultimately passed to `ra:pipeline_command/4` and each converter module
    calls `rabbit_db_m2k_converter:with_correlation_id/2` on the state to
    get a new correlation ID and add an async request. Each async change is
    briefly attached to the state record as a function so that requests can
    be retried in the case that the Ra leader changes.
    `with_correlation_id/2` also acts as a backpressure mechanism so we
    avoid flooding the Ra process with messages.
    
    I've set the maximum for in-flight requests to 64 to start with. The
    worker pool for importing definitions has 32 members, so migration tends
    to be faster than initial definition import. This could be tuned based
    on memory/CPU usage however.

--- deps/rabbit/src/rabbit_db_m2k_converter.erl
+++ deps/rabbit/src/rabbit_db_m2k_converter.erl
@@ -14,17 +14,78 @@
 -include_lib("khepri_mnesia_migration/src/kmm_logging.hrl").
 -include_lib("rabbit_common/include/rabbit.hrl").
 
+%% Functions for `rabbit_db_*_m2k_converter' modules to call.
+-export([with_correlation_id/2,
+         get_store_id/1]).
+
+%% `mnesia_to_khepri_converter' callbacks.
 -export([init_copy_to_khepri/4,
          copy_to_khepri/3,
-         delete_from_khepri/3]).
+         delete_from_khepri/3,
+         finish_copy_to_khepri/1]).
+
+-define(MAX_ASYNC_REQUESTS, 64).
 
 -type migration() :: {mnesia_to_khepri:mnesia_table(),
                       mnesia_to_khepri:converter_mod()}.
 
 -type migrations() :: [migration()].
 
--record(?MODULE, {migrations :: migrations(),
-                  sub_states :: #{module() => any()}}).
+-type correlation_id() :: non_neg_integer().
+
+-type async_request_fun() :: fun((correlation_id()) -> ok | {error, any()}).
+
+-record(?MODULE, {store_id :: khepri:store_id(),
+                  migrations :: migrations(),
+                  sub_states :: #{module() => any()},
+                  seq_no = 0 :: correlation_id(),
+                  last_acked_seq_no = 0 :: correlation_id(),
+                  async_requests = #{} :: #{correlation_id() =>
+                                            async_request_fun()}}).
+
+-opaque state() :: #?MODULE{}.
+
+-export_type([state/0]).
+
+-spec with_correlation_id(Fun, State) -> Ret when
+      Fun :: async_request_fun(),
+      State :: state(),
+      Ret :: {ok, NewState} | {error, Reason},
+      NewState :: state(),
+      Reason :: any().
+
+with_correlation_id(
+  Fun,
+  #?MODULE{seq_no = SeqNo0,
+           last_acked_seq_no = LastAckedSeqNo,
+           async_requests = AsyncRequests0} = State0) ->
+    case SeqNo0 - LastAckedSeqNo >= ?MAX_ASYNC_REQUESTS of
+        true ->
+            case wait_for_async_requests(State0) of
+                {ok, State} ->
+                    with_correlation_id(Fun, State);
+                {error, _} = Error ->
+                    Error
+            end;
+        false ->
+            SeqNo = SeqNo0 + 1,
+            AsyncRequests = AsyncRequests0#{SeqNo => Fun},
+            State = State0#?MODULE{seq_no = SeqNo,
+                                   async_requests = AsyncRequests},
+            case Fun(SeqNo) of
+                ok ->
+                    {ok, State};
+                {error, _} = Error ->
+                    Error
+            end
+    end.
+
+-spec get_store_id(state()) -> khepri:store_id().
+
+get_store_id(#?MODULE{store_id = StoreId}) ->
+    StoreId.
+
+%% `mnesia_to_khepri_converter' callbacks
 
 -spec init_copy_to_khepri(StoreId, MigrationId, Tables, Migrations) ->
     Ret when
@@ -63,64 +124,131 @@ init_copy_to_khepri(StoreId, MigrationId, _Tables, Migrations) ->
                           Acc#{Mod => SubState}
                   end, #{}, TablesPerMod),
 
-    State = #?MODULE{migrations = Migrations,
+    State = #?MODULE{store_id = StoreId,
+                     migrations = Migrations,
                      sub_states = SubStates},
     {ok, State}.
 
--spec copy_to_khepri(Table, Record, Priv) -> Ret when
+-spec copy_to_khepri(Table, Record, State) -> Ret when
       Table :: mnesia_to_khepri:mnesia_table(),
       Record :: tuple(),
-      Priv :: #?MODULE{},
-      Ret :: {ok, NewPriv} | {error, Reason},
-      NewPriv :: #?MODULE{},
+      State :: state(),
+      Ret :: {ok, NewState} | {error, Reason},
+      NewState :: state(),
       Reason :: any().
 %% @private
 
 copy_to_khepri(
-  Table, Record,
-  #?MODULE{migrations = Migrations, sub_states = SubStates} = State) ->
+  Table, Record, #?MODULE{migrations = Migrations} = State) ->
     case proplists:get_value(Table, Migrations) of
         true ->
             {ok, State};
         Mod when Mod =/= undefined ->
             ActualMod = actual_mod(Mod),
-            SubState = maps:get(Mod, SubStates),
-            case ActualMod:copy_to_khepri(Table, Record, SubState) of
-                {ok, SubState1} ->
-                    SubStates1 = SubStates#{Mod => SubState1},
-                    State1 = State#?MODULE{sub_states = SubStates1},
+            case ActualMod:copy_to_khepri(Table, Record, State) of
+                {ok, State1} ->
                     {ok, State1};
                 {error, _} = Error ->
                     Error
             end
     end.
 
--spec delete_from_khepri(Table, Key, Priv) -> Ret when
+-spec delete_from_khepri(Table, Key, State) -> Ret when
       Table :: mnesia_to_khepri:mnesia_table(),
       Key :: any(),
-      Priv :: #?MODULE{},
-      Ret :: {ok, NewPriv} | {error, Reason},
-      NewPriv :: #?MODULE{},
+      State :: state(),
+      Ret :: {ok, NewState} | {error, Reason},
+      NewState :: state(),
       Reason :: any().
 %% @private
 
 delete_from_khepri(
-  Table, Key,
-  #?MODULE{migrations = Migrations, sub_states = SubStates} = State) ->
+  Table, Key, #?MODULE{migrations = Migrations} = State) ->
     case proplists:get_value(Table, Migrations) of
         true ->
             {ok, State};
         Mod when Mod =/= undefined ->
             ActualMod = actual_mod(Mod),
-            SubState = maps:get(Mod, SubStates),
-            case ActualMod:delete_from_khepri(Table, Key, SubState) of
-                {ok, SubState1} ->
-                    SubStates1 = SubStates#{Mod => SubState1},
-                    State1 = State#?MODULE{sub_states = SubStates1},
+            case ActualMod:delete_from_khepri(Table, Key, State) of
+                {ok, State1} ->
                     {ok, State1};
                 {error, _} = Error ->
                     Error
             end
+    end.
+
+-spec finish_copy_to_khepri(State) -> Ret when
+      State :: state(),
+      Ret :: ok.
+%% @private
+
+finish_copy_to_khepri(State) ->
+    {ok, _} = wait_for_all_async_requests(State),
+    ok.
+
+wait_for_all_async_requests(
+  #?MODULE{seq_no = SeqNo,
+           last_acked_seq_no = LastAckedSeqNo} = State) ->
+    case SeqNo - LastAckedSeqNo > 0 of
+        true ->
+            case wait_for_async_requests(State) of
+                {ok, State1} ->
+                    wait_for_all_async_requests(State1);
+                {error, _} = Error ->
+                    Error
+            end;
+        false ->
+            {ok, State}
+    end.
+
+wait_for_async_requests(
+  #?MODULE{store_id = StoreId,
+           seq_no = SeqNo0,
+           async_requests = AsyncRequests0} = State0) ->
+    receive
+        {ra_event, _CurrentLeader, {applied, Correlations}} ->
+            lists:foldl(
+              fun (_, {error, _} = Error) ->
+                      Error;
+                  ({SeqNo, Resp}, {ok, State}) ->
+                      #?MODULE{async_requests = AsyncRequests,
+                               last_acked_seq_no = LastAcked} = State,
+                      LastAcked1 = erlang:max(SeqNo, LastAcked),
+                      AsyncRequests1 = maps:remove(SeqNo, AsyncRequests),
+                      State1 = State#?MODULE{last_acked_seq_no = LastAcked1,
+                                             async_requests = AsyncRequests1},
+                      case Resp of
+                          ok ->
+                              {ok, State1};
+                          {ok, _} ->
+                              {ok, State1};
+                          {error, _} = Error ->
+                              Error;
+                          {exception, _, _, _} = Exception ->
+                              khepri_machine:handle_tx_exception(
+                                Exception)
+                      end
+              end, {ok, State0}, Correlations);
+        {ra_event,
+         FromId,
+         {rejected, {not_leader, MaybeLeader, FailedSeqNo}}} ->
+            %% If the leader changes during an async request, retry the failed
+            %% request against the new leader.
+            ok = khepri_cluster:cache_leader_if_changed(
+                   StoreId, FromId, MaybeLeader),
+            {Fun, AsyncRequests} = maps:take(FailedSeqNo, AsyncRequests0),
+            SeqNo = SeqNo0 + 1,
+            AsyncRequests1 = AsyncRequests#{SeqNo => Fun},
+            State = State0#?MODULE{seq_no = SeqNo,
+                                   async_requests = AsyncRequests1},
+            case Fun(SeqNo) of
+                ok ->
+                    {ok, State};
+                {error, _} = Error ->
+                    Error
+            end
+    after 5_000 ->
+            {error, timeout}
     end.
 
 actual_mod({Mod, _}) -> Mod;
